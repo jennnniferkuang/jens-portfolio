c577b0481ad50a56b6a0f0fafb4483ece0eb81b5
jennnniferkuang <94874342+jennnniferkuang@users.noreply.github.com>
2025-08-04 00:04:13 -0400

    Added flowbite component library!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 // import Image from "next/image";
 import Footer from '@/components/footer';
 import HorizontalScrollGallery from '@/components/horizontal-scroll-gallery';
+import { Button } from 'flowbite-react';
 
 export default function Home() {
   return (
@@ -10,8 +11,8 @@ export default function Home() {
         <h3>Fun Fact!</h3>
         <p className='justify-center'>This site is a tribute to my first EVER game called Out of Sight, a 2D horror platformer. Totally atrocious piece of code, but it still means a lot to me to this day! You can check it out here:</p>
         <div className='flex flex-row gap-4'>
-          <button>Demo</button>
-          <button>Repo</button>
+          <Button>Demo</Button>
+          <Button>Repo</Button>
         </div>
       </div>
       <Footer/>
