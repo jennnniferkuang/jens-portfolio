bd9c18e9d24ac26365dd64ca020f488b201d2abd
Jennifer Kuang <jkuang006@gmail.com>
2025-08-15 10:16:44 -0400

    yet anotehr build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,8 +16,8 @@ export default function Home() {
         <h3>Fun Fact!</h3>
         <p className='justify-center'>This site is a tribute to my first EVER game called Out of Sight, a 2D horror platformer. Totally atrocious piece of code, but it still means a lot to me to this day! You can check it out here:</p>
         <div className='flex flex-row gap-4'>
-          <Button color='dark' href="https://youtu.be/wUkGteWnN54" target="_blank">Demo</Button>
-          <Button color='dark' href="https://github.com/jennnniferkuang/Out-of-Sight" target="_blank">Repo</Button>
+          <Button as="a" color='dark' href="https://youtu.be/wUkGteWnN54" target="_blank">Demo</Button>
+          <Button as="a" color='dark' href="https://github.com/jennnniferkuang/Out-of-Sight" target="_blank">Repo</Button>
         </div>
       </div>
       <Footer/>
